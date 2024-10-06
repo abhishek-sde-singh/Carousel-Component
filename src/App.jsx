8c457d951351f9d13644ec388b205161b922f962
Abhishek Singh <abhi@Abhisheks-MacBook-Pro.local>
2024-10-06 15:42:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center w-full h-[100vh] bg-gray-500">
+      <h1 className="text-5xl text-teal-300">React Carousel</h1>
+    </div>
+  );
+}
+
+export default App;
